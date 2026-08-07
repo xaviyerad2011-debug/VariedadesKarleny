5ac5e46a813f0ec71b2bff2222554db284403a53
xaviyerad2011-debug <xaviyerad2011@gmail.com>
2026-08-07 16:12:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ir a la sección de productos
+function mostrarMensaje() {
+    const productos = document.getElementById("productos");
+
+    productos.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+console.log("✅ Variedades Karleny funcionando correctamente");
