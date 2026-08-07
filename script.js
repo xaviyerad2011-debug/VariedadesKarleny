45425336d05ccb066296ccf8a0e93148bd67b595
xaviyerad2011-debug <xaviyerad2011@gmail.com>
2026-08-07 16:37:55 -0500

    Implement WhatsApp purchase function
    Added a function to facilitate WhatsApp purchases.

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function mostrarMensaje() {
 }
 
 console.log("✅ Variedades Karleny funcionando correctamente");
+function comprarWhatsApp(nombre, precio) {
+
+    const numero = "573202104423";
+
+    const mensaje =
+        "Hola 👋, estoy interesado en comprar este producto:%0A%0A" +
+        "🛍️ Producto: " + nombre + "%0A" +
+        "💰 Precio: $" + precio;
+
+    const enlace = "https://wa.me/" + numero + "?text=" + mensaje;
+
+    window.open(enlace, "_blank");
+}
